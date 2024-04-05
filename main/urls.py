61b1e4da11f908538dfeda236ef939bc0601f2a3
Иван Афанасов <afanasov711@gmail.com>
2024-04-05 22:25:57 +0300

    Advices added

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('testpage', views.testpage, name='testpage'),
+    path('advices', views.advices, name='advices'),
+    path('materials', views.materials, name='materials'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 
 def index(request):
     return render(request, 'index.html', {})
     # return HttpResponse("Hello, world. You're at the polls index.")
 
+def testpage(request):
+    return render(request, 'testpage.html', {})
+
+def advices(request):
+    return render(request, 'advices.html', {})
+
+def materials(request):
+    return render(request, 'materials.html', {})
